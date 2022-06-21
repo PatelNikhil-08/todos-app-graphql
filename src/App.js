a754258d587d92f0c3e60179ae073f2972def47b
Nikhil-cloud-sudo <patelnikhil8996@gmail.com>
2022-06-21 00:44:24 -0400

    add the strike feature and the toggle todo feature and add todo feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react'; 
-import {useQuery, gql} from '@apollo/client';
+import {useQuery, gql, useMutation} from '@apollo/client';
 
 
 const GET_TODOS = gql`
@@ -11,25 +11,86 @@ query MyQuery {
     text
   }
 }
+`;
 
-`
+const TOGGLE_TODO = gql`
+mutation toggleTodo($id: uuid!, $done:Boolean!) {
+  update_todos(where: {id: { _eq: $id }}, _set: { done: $done }) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+`;
+
+
+const ADD_TODO = gql`
+mutation addTodo($text: String!) {
+  insert_todos(objects: {text: $text}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+`;
+
+
+
 function App() {
+  const [text, setText] = React.useState(); 
   const {data, loading, error} = useQuery(GET_TODOS); 
+  const[toggleTodo] =  useMutation(TOGGLE_TODO);
+  const[addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () => setText("")
+  });
+
+
+  async function handleToggleTodo({id, done}){
+    await toggleTodo({variables: {id, done: !done}})
+      
+  }
+
+  async function handleAddTodo(event){
+    event.preventDefault();
+    if(!text.trim()) return;
+      await addTodo({variables: {text: text}})
+   
+
+  }
+
   if(loading) return <div>loading todos....</div>
   if(error) return <div>Error fetching todos</div>
 
    
   return (
-    <div>
-      <h1>Basic TODO'S Application using GRAPHQL </h1>
-      {data.todos.map(todo => (
-        <p key={todo.id}>
-          <span>
-            {todo.text}
-          </span>
-          <button>&times;</button>
-        </p>
-      ))}
+    <div className="vh-100 code flex flex-column items-center pa4 fl-1">
+      <h1 className="f2-l"> TODO'S Application {" "}
+      <span role="img" aria-label="checkmark"></span>
+      </h1>
+      <form className="mb3" onSubmit={handleAddTodo}>
+        <input
+          className="pa2 f4 b-dashed"
+          type="text"
+          placeholder='New Todo'
+          onChange={event => setText(event.target.value)}
+          value={text}
+        />
+        <button className="pa2 f4" type='submit'>Create</button>
+      </form>
+      <div className="flex items-center justify-center flex-column">
+        {data.todos.map(todo => (
+          <p onDoubleClick={()=>handleToggleTodo(todo)} key={todo.id}>
+            <span  className={`${todo.done && 'strike'}`}>
+              {todo.text}
+            </span>
+            <button>&times;</button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
